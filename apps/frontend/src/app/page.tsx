dc26aad07f7b1691706c750dbb1100119d77d7b3
Afuye Iyiola <iyiolad21@gmail.com>
2025-10-07 08:41:08 +0100

    feat(frontend): Testing

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -6,7 +6,13 @@ export default function Home() {
   const isMobile = useMobile();
   return (
     <main>
-      <div>{isMobile ? <p>Mobile View</p> : <p>Desktop View</p>}</div>
+      <div>
+         {isMobile ? (
+          <p className="flex items-center justify-center min-h-screen">Mobile View</p>
+        ) : (
+          <p className="flex items-center justify-center min-h-screen">Desktop View</p>
+        )}
+      </div>
     </main>
   );
 }
