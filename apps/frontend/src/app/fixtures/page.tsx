8c6faf1f84c3168b30a836a8689fa7ed7473fb43
Afuye Iyiola <iyiolad21@gmail.com>
2025-10-16 09:54:43 +0100

    feat: add SportsType component and integrate into FixturesPage with tabs for sport selection

--- apps/frontend/src/app/fixtures/page.tsx
+++ apps/frontend/src/app/fixtures/page.tsx
@@ -1,12 +1,29 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components';
+import { SportsTypeTabs } from './components';
 import { FixturesList } from './components/fixtures-list';
 
 export default function FixturesPage() {
   const today = new Date().toISOString().split('T')[0];
   return (
-    <div className="flex-1 p-4 max-w-4xl mx-auto bg-app-background ">
+    <div className="flex-1 p-4 max-w-4xl mx-auto bg-app-background gap-6 flex flex-col">
       <div className="mb-6">
-        <h1 className="text-3xl font-bold mb-2">Fixtures</h1>
-        <p className="text-gray-600">Today's matches grouped by league</p>
+        <div className="border-y-1 border-grey-300 py-4">
+          <SportsTypeTabs />
+        </div>
+        <div className='mt-2'>
+          <h1 className="text-2xl font-semibold ">Fixtures</h1>
+          <div className="mt-2 w-[100px]" >
+            <Select defaultValue={today}  name="fixture-date">
+              <SelectTrigger size="sm">
+                <SelectValue placeholder="Filter options" className='text-grey-950' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="one">All fixtures</SelectItem>
+                <SelectItem value="two">Upcoming fixtures</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
       </div>
       <FixturesList initialDate={today} />
     </div>
